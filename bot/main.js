1a0926e36885e170a63475ead4733bccc9eb0cbb
Sarah Schulte <git@sarah.engineer>
2023-10-14 00:36:49 -0700

    add bit golf support and refactor

--- bot/main.js
+++ bot/main.js
@@ -1,6 +1,8 @@
 import { Client, Events, GatewayIntentBits } from "discord.js";
 import "dotenv/config";
 import { readFileSync, writeFile } from "fs";
+import EventSource from "eventsource";
+import { throttle } from "throttle-debounce";
 
 const client = new Client({
   intents: [
@@ -14,50 +16,75 @@ client.once(Events.ClientReady, (c) => {
   console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
+const nothing = () => {};
+const queue = [];
+
+const updateScore = (who, score, cb = nothing) => {
+  queue.push([who, score, cb]);
+  updateBatch();
+};
+
+const updateBatch = throttle(1000, () => {
+  let file = [];
+  try {
+    file = readFileSync("../data/data.txt", {
+      encoding: "utf8",
+      flag: "r",
+    });
+    file = file.split("\n");
+  } catch (e) {
+    console.log("Re-creating data.txt ...");
+  }
+
+  queue.forEach(([who, score, cb]) => {
+    let handled = false;
+
+    file.forEach((row, index) => {
+      const rowData = row.split("|");
+      if (rowData.length <= 1) return;
+
+      if (rowData[0] == who) {
+        if (parseInt(rowData[1]) < parseInt(score)) {
+          rowData[1] = score;
+          cb("✅");
+          handled = true;
+          file[index] = rowData.join("|");
+        } else {
+          cb("🔽");
+          handled = true;
+        }
+      }
+    });
+
+    if (!handled) {
+      file[file.length - 1] = `${who}|${score}`;
+      file.push("");
+      cb("✅");
+    }
+  });
+
+  writeFile("../data/data.txt", file.join("\n"), (err) => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+  });
+});
 client.on(Events.MessageCreate, (message) => {
   if (message.channelId == "964243131656716288") {
     if (isNaN(message.content)) {
       message.react("❌");
     } else {
-      let file = readFileSync("../data/data.txt", {
-        encoding: "utf8",
-        flag: "r",
-      });
-
-      file = file.split("\n");
-      let handled = false;
-
-      file.forEach((row, index) => {
-        const rowData = row.split("|");
-        if (rowData.length <= 1) return;
-
-        if (rowData[0] == message.author.id) {
-          if (parseInt(rowData[1]) < parseInt(message.content)) {
-            rowData[1] = message.content;
-            message.react("✅");
-            handled = true;
-            file[index] = rowData.join("|");
-          } else {
-            message.react("🔽");
-            handled = true;
-          }
-        }
-      });
-
-      if (!handled) {
-        file[file.length - 1] = `${message.author.id}|${message.content}`;
-        file.push("");
-        message.react("✅");
-      }
-
-      writeFile("../data/data.txt", file.join("\n"), (err) => {
-        if (err) {
-          console.error(err);
-        }
-        // file written successfully
-      });
+      updateScore(message.author.id, message.content, message.react);
     }
   }
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+// connect to bit golf API
+const es = new EventSource("https://ld54.badcop.games/event/sse");
+es.addEventListener("update", (e) => {
+  let [who, score] = e.data.split(":");
+  updateScore(who, -parseInt(score));
+});
